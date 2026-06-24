94bfd21d91b4702d3e47c35c4598bee65dd9cf53
Cyrus Bahrambegi <cyrusb2007@gmail.com>
2026-06-23 23:48:07 -0400

    Add about and contact pages with shared navigation

--- orpus1/headers.js
+++ orpus1/headers.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
       <h2>The Future Of Supply Tracking</h2>
       <nav>
         <a class="tablink" href="test.html">Home</a>
+        <a class="tablink" href="about.html">About</a>
         <a class="tablink" href="meetUs.html">Meet Us</a>
         <a class="tablink" href="contact.html">Contact</a>
       </nav>
